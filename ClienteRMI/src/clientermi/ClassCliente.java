5170c4399db269da57a6394de0b91d5776b08dd3
Camilacat05 <37548708+Camilacat05@users.noreply.github.com>
2019-03-20 19:23:00 -0300

    Todas as classes do cliente
    Com a interface e a chamada do cliente

---
+++ ClienteRMI/src/clientermi/ClassCliente.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clientermi;
+
+import RMIInterface.Interfaces;
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+
+/**
+ *
+ * @author Sam
+ */
+public class ClassCliente {
+    public Interfaces init() {
+
+        String remoteHostName = "192.168.43.21";
+        int remotePort = 9002;
+        String connectLocation = "//" + remoteHostName + ":" + remotePort + "/Hello";
+
+        Interfaces hello = null;
+        try {
+            System.out.println("\nConnecting to client at : " + connectLocation);
+            hello = (Interfaces) Naming.lookup(connectLocation);
+        } catch (MalformedURLException | RemoteException | NotBoundException e1) {}
+        
+        return hello;
+    }
+}
